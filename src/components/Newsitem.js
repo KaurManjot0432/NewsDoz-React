4e576a99363d149f2de3bf1b87a5b7060ce4cbbe
KaurManjot0432 <0432manjot@gmail.com>
2021-09-24 21:38:08 +0530

    news categorized

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -8,10 +8,10 @@ export class Newsitem extends Component {
             <div my-3>
                 <div className="card">
                     <img src={!imgURL?"https://cdn.insidesport.co/wp-content/uploads/2021/09/15113727/WhatsApp-Image-2021-09-15-at-11.08.38-AM.jpeg" : imgURL} className="card-img-top" alt="..."/>
-                    <div class ="card-body">
-                    <h5 class ="card-title">{title}</h5>
-                    <p class ="card-text">{description}</p>
-                    <a href={newsURL}   target="_blank" class ="btn btn-sm btn-dark">Read more</a>
+                    <div className = "card-body">
+                    <h5 className = "card-title">{title}</h5>
+                    <p className = "card-text">{description}</p>
+                    <a href={newsURL}   target="_blank" className = "btn btn-sm btn-dark">Read more</a>
                     </div>
                 </div>
             </div>
